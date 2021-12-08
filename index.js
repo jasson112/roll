172f7747c9fa9145242febbb09b93faaa33dfe7f
Jasson <jasson142@gmail.com>
2021-12-08 01:51:53 -0500

    adding web server

--- index.js
+++ index.js
@@ -32,3 +32,15 @@ client.on('messageCreate', (message) => {
 })
 
 client.login(process.env.BOT_TOKEN)
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
